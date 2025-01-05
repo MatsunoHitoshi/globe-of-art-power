edf67ae57beaf6b102a6e0884baa126ae2446483
松野仁志 <MatsunoHitoshi1109Kumamoto@gmail.com>
2025-01-05 11:23:47 +0900

    ページのパラメータ修正

--- src/app/peppers-ghost/[id]/page.tsx
+++ src/app/peppers-ghost/[id]/page.tsx
@@ -1,13 +1,15 @@
 import type { NextPage } from "next";
 import { PeppersGhostPageContent } from "../../_components/peppers-ghost-page-content";
 
-type PageParams = { params: { id: string } };
+interface PageParams {
+  params: Promise<{ id: string }>;
+}
 
 const Page: NextPage<PageParams> = async ({ params }: PageParams) => {
-  const controlId = params.id;
+  const { id } = await params;
   return (
     <>
-      <PeppersGhostPageContent controlId={controlId} />
+      <PeppersGhostPageContent controlId={id} />
     </>
   );
 };
